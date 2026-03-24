147bbf45863d499302effe9091242ae912a73c54
Michael <michael@Michaels-MacBook-Pro.local>
2026-03-24 13:37:08 +0000

    feat: set design system as default page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import { Toaster } from "@/components/ui/sonner";
-import { Layout } from "@/components/layout/Layout";
-import { DevReviewPage } from "@/pages/DevReviewPage";
 import { DesignSystemPage } from "@/pages/DesignSystemPage";
 
 export default function App() {
-  const path = window.location.pathname;
-
   return (
     <>
       <Toaster />
-      {path === '/design-system' ? (
-        <DesignSystemPage />
-      ) : (
-        <Layout>
-          <DevReviewPage />
-        </Layout>
-      )}
+      <DesignSystemPage />
     </>
   );
 }
